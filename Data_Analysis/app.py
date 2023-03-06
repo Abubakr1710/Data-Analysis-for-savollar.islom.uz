f31d4fcb227d437d11bb48e5fe05430f9e99e180
Abubakr1710 <mamajonov1710@gmail.com>
2023-03-06 06:17:18 +0100

    feature added

--- Data_Analysis/app.py
+++ Data_Analysis/app.py
@@ -3,11 +3,39 @@ import pandas as pd # Importing pandas, a python library for data analysis.
 import matplotlib.pyplot as plt # Importing matplotlib, a python library for data visualization.
 import seaborn as sns # Importing seaborn, a python library for data visualization.
 from password import pcode # Importing the password.py file.
+from graph_page import show_data, graph_plot, graph_plot2 # Importing the graph_page.py file.
+from PIL import Image # Importing the PIL library for image processing.
+
 header = st.container() # Creating a container for the header.
 dataset = st.container() # Creating a container for the dataset.
 features = st.container() # Creating a container for the features.
 
 with header: # Creating a header.
-    st.markdown("<h1 style='text-align: center; color: black;'>DATA ANALYSIS</h1>", unsafe_allow_html=True)
-    pcode() # Calling the password function from the password.py file.
+    image = Image.open('assets/logo.png') # Opening the logo image.
+    st.sidebar.image(image, use_column_width=True) # Displaying the logo image.
+    st.sidebar.markdown("<h1 style='text-align: center; color: black;'>DATA TEAM</h1>", unsafe_allow_html=True) # Creating a header.
+    menu = ["Bosh Sahifa", "Ma'lumotlar Tahlili", "Jamoa Haqida", "Aloqa"]
+    choice = st.sidebar.selectbox('Menu', menu)
+    if choice == "Bosh Sahifa":
+        st.markdown("<h1 style='text-align: center; color: black;'>BOSH SAHIFA</h1>", unsafe_allow_html=True) # Creating a header.
+        st.text("Test rejimida ishlamoqda")
+    
+    elif choice == "Ma'lumotlar Tahlili":
+        st.markdown("<h1 style='text-align: center; color: black;'>MA'LUMOTAR TAHLILI</h1>", unsafe_allow_html=True) # Creating a header.
+        if pcode() is True:
+            show_data()
+            graph_plot()
+            graph_plot2()
+
+            
+    elif choice == "Jamoa Haqida":
+        st.markdown("<h1 style='text-align: center; color: black;'>JAMOA HAQIDA</h1>", unsafe_allow_html=True)
+        st.text("Test rejimida ishlamoqda")
+    
+    elif choice == "Aloqa":
+        st.markdown("<h1 style='text-align: center; color: black;'>ALOQA</h1>", unsafe_allow_html=True)
+        st.text("Test rejimida ishlamoqda")
+
+        
+
 
