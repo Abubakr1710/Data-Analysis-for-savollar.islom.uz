c4879e4b939d958acd15e589c920a77251abe8dc
Abubakr1710 <mamajonov1710@gmail.com>
2023-03-06 06:55:25 +0100

    updated

--- Data_Analysis/app.py
+++ Data_Analysis/app.py
@@ -11,8 +11,8 @@ dataset = st.container() # Creating a container for the dataset.
 features = st.container() # Creating a container for the features.
 
 with header: # Creating a header.
-    image = Image.open('https://github.com/Abubakr1710/Data-Analysis-for-savollar.islom.uz/blob/main/Data_Analysis/assets/picture.png') # Opening the logo image.
-    st.sidebar.image(image, use_column_width=True) # Displaying the logo image.
+    #image = Image.open('https://github.com/Abubakr1710/Data-Analysis-for-savollar.islom.uz/blob/main/Data_Analysis/assets/picture.png') # Opening the logo image.
+    st.sidebar.image('https://github.com/Abubakr1710/Data-Analysis-for-savollar.islom.uz/blob/main/Data_Analysis/assets/picture.png', use_column_width=True) # Displaying the logo image.
     st.sidebar.markdown("<h1 style='text-align: center; color: black;'>DATA TEAM</h1>", unsafe_allow_html=True) # Creating a header.
     menu = ["Bosh Sahifa", "Ma'lumotlar Tahlili", "Jamoa Haqida", "Aloqa"]
     choice = st.sidebar.selectbox('Menu', menu)
