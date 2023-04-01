8cc8ab5a230f6c798c307f6aa244b68b892957e2
Abubakr Mamajonov <mamajonov1710@gmail.com>
2023-04-01 02:33:40 +0200

    updated

--- Data_Analysis/test.py
+++ Data_Analysis/test.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-df = pd.read_csv('new_data.csv')
+
+df = pd.read_csv('./new_data.csv')
 udf = df.copy()
 ydf = df.copy()
 bdf = df.copy()
